63583530cc1c88cfb2967ebd3ce0c76d5ed89c7d
Stone-Monkey1 <92111289+Stone-Monkey1@users.noreply.github.com>
2026-02-16 10:00:15 -0500

    Add project files.

---
+++ MoreWeaponDamage/SwordDamage.cs
@@ -0,0 +1,28 @@
+﻿
+using MoreWeaponDamage;
+
+namespace CalculateDamage
+{
+    internal class SwordDamage : WeaponDamage
+    {
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE = 2;
+
+        public SwordDamage(int startingRoll) : base(startingRoll) { }
+
+        protected override void CalculateDamage()
+        {
+            decimal MagicMultiplier = 1M;
+            if (Magic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE;
+            if (Flaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+    }
+}
---
+++ MoreWeaponDamage/WeaponDamage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreWeaponDamage
+{
+    internal class WeaponDamage
+    {
+        public int Damage { get; protected set; }
+        public int roll;
+        public bool flaming;
+        public bool magic;
+
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+
+
+        public bool Flaming { get { return flaming; } set { flaming = value; CalculateDamage(); } }
+
+        public bool Magic { get { return magic; } set { magic = value; CalculateDamage(); } }
+
+
+        public WeaponDamage(int startingRoll)
+        {
+            roll = startingRoll;
+            CalculateDamage();
+        }
+
+        protected virtual void CalculateDamage()
+        {
+            Console.WriteLine("This wasn't overwritten for some reason");
+        }
+
+    }
+}
